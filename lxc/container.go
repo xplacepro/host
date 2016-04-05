f341649d613e570ee75699a64dfd984b129af322
Alex Sofin <sofin.moffin@gmail.com>
2016-04-05 14:53:31 +0300

    container pass

--- lxc/container.go
+++ lxc/container.go
@@ -151,9 +151,10 @@ func (c *Container) RamUsage() (int, error) {
 	return strconv.Atoi(strings.Trim(out, "\n"))
 }
 
-func (c *Container) Create(template string, fssize int, vgname string, config string) (string, error) {
+func (c *Container) Create(template string, fssize int, vgname string, config string, user string, password string) (string, error) {
 	out, err := common.RunCommand(path.Join(LXC_BIN, "lxc-create"), []string{"-n", c.Name,
-		"-t", template, "-B", "lvm", fmt.Sprintf("--fssize=%vb", fssize), fmt.Sprintf("--vgname=%s", vgname)})
+		"-B", "lvm", fmt.Sprintf("--fssize=%vb", fssize), fmt.Sprintf("--vgname=%s", vgname),
+		"-t", template, "--", fmt.Sprintf("--user=%s", user), fmt.Sprintf("--password=%s", password)})
 	if err := c.AppendConfig(config); err != nil {
 		return out, err
 	}
